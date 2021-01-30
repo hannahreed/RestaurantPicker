2a0abc5ed81fff0a0576e27b62062e5913a59236
Hannah Reed <hreed277@gmail.com>
2021-01-30 15:33:33 -0500

    use fetch

--- src/App.js
+++ src/App.js
@@ -19,23 +19,14 @@ class App extends React.Component {
   handleKeyPress(event){
     if(event.key == 'Enter') {
       console.log("Enter was pressed");
-      console.log("hello");
-      var auth = 'AIzaSyCMBejhSp6tAA-1V5rAa36O7CJ5A1pHkfA';
-      var body = "";
-      var endpoint = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + this.state.searchString + '&key=AIzaSyCMBejhSp6tAA-1V5rAa36O7CJ5A1pHkfA';
-      let request = new XMLHttpRequest();
-      request.open("GET", endpoint);
-      request.send();
-      request.onload = () => {
-        console.log(request);
-        if (request.status == 200) {
-          body = JSON.parse(request.response);
-          console.log(body);
-        } else {
-          console.log(`error ${request.status} ${request.statusText}`);
-        }
-      }
+
+      // fetch address information
+      var addressData;
+      fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + this.state.searchString + '&key=AIzaSyCMBejhSp6tAA-1V5rAa36O7CJ5A1pHkfA')
+        .then(response => response.json())
+        .then(data => console.log(data));
     }
+    
   }
 
   handleChange(event) {
